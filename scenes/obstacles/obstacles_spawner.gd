1b03d918867135235721aeb2d832df62a2d5c9b7
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-11-29 23:47:28 +0200

    obstacles spawner

---
+++ scenes/obstacles/obstacles_spawner.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var spawn_point: Node2D
+@export var obstacles_array: Node2D
+
+const OBSTACLE_Y_DELTA = 100
+
+var ObstacleScene: PackedScene = preload("res://scenes/obstacles/obstacles.tscn")
+
+func _ready() -> void:
+	_spawn_new_obstacle()
+
+func _on_spawn_timer_timeout() -> void:
+	_spawn_new_obstacle()
+
+func _spawn_new_obstacle() -> void:
+	var obstacle = ObstacleScene.instantiate()
+	obstacle.global_position.x = spawn_point.global_position.x
+	obstacle.global_position.y = randi_range(-OBSTACLE_Y_DELTA, OBSTACLE_Y_DELTA)
+	obstacles_array.add_child(obstacle)
