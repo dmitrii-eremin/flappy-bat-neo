c63c96913fde837e3f6be97260e0da0327866c18
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-11-30 23:35:11 +0200

    added credits

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -15,4 +15,4 @@ func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_credits_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/menu/menu_credits.tscn")
