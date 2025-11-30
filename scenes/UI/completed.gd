b3241a5be1d4c472e852212defca8c36433c24a8
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-11-30 16:03:24 +0200

    added buttons

--- scenes/UI/completed.gd
+++ scenes/UI/completed.gd
@@ -8,3 +8,10 @@ func _ready() -> void:
 
 func _on_turn_bat_timer_timeout() -> void:
 	_bat_sprite.flip_h = not _bat_sprite.flip_h
+
+func _on_play_again_button_pressed() -> void:
+	GameData.gained_score = 0
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().quit()
