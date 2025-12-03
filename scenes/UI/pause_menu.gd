630bf1f9d127ca1af1b3d78fbe9646d64bff0dd6
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-12-03 20:32:53 +0200

    save record after pause

--- scenes/UI/pause_menu.gd
+++ scenes/UI/pause_menu.gd
@@ -2,10 +2,14 @@ extends CanvasLayer
 
 signal on_resume()
 
+@onready var _user_data = UserData.new()
+
 func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
 	on_resume.emit()
 
 func _on_main_menu_button_pressed() -> void:
+	_user_data.update_score(GameData.gained_score)
+
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")
